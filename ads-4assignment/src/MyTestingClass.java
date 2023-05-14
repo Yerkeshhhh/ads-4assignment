c2bb9a29b5a15c35679e0d18be9bf622df6a2a6c
Yerkeshhhh <117143038+Yerkeshhhh@users.noreply.github.com>
2023-05-14 13:14:20 +0600

    d

--- ads-4assignment/src/MyTestingClass.java
+++ ads-4assignment/src/MyTestingClass.java
@@ -1,2 +1,20 @@
 public class MyTestingClass {
+    private int id;
+    private String name;
+
+    public MyTestingClass(int id, String name) {
+        this.id=id;
+        this.name=name;
+    }
+
+    public int hashCode() {
+        int result=17;
+        result=31*result+id;
+        result=31*result+name.hashCode();
+        return result;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
