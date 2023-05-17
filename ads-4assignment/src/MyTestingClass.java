5cdd9e5063ef850a098f18891f05cd35761e8a4f
Yerkeshhhh <117143038+Yerkeshhhh@users.noreply.github.com>
2023-05-17 08:42:18 +0600

    d

--- ads-4assignment/src/MyTestingClass.java
+++ ads-4assignment/src/MyTestingClass.java
@@ -9,7 +9,6 @@ public class MyTestingClass {
 
     @Override
     public int hashCode() {
-        // Custom hashCode implementation
         int prime = 31;
         int result = 1;
         result = prime * result + id;
@@ -19,7 +18,6 @@ public class MyTestingClass {
 
     @Override
     public boolean equals(Object obj) {
-        // Custom equals implementation
         if (this == obj) {
             return true;
         }
@@ -38,7 +36,6 @@ public class MyTestingClass {
     public static void main(String[] args) {
         MyHashTable<MyTestingClass, String> table = new MyHashTable<>();
 
-        // Put elements in the hashtable
         MyTestingClass key1 = new MyTestingClass(1, "Element1");
         String value1 = "Value1";
         table.put(key1, value1);
@@ -51,21 +48,16 @@ public class MyTestingClass {
         String value3 = "Value3";
         table.put(key3, value3);
 
-        // Get values using keys
         System.out.println("Value for key1: " + table.get(key1));
         System.out.println("Value for key2: " + table.get(key2));
         System.out.println("Value for key3: " + table.get(key3));
 
-        // Remove an element
         table.remove(key2);
 
-        // Check if a value exists in the hashtable
         System.out.println("Contains value 'Value2': " + table.containsValue("Value2"));
 
-        // Get the key for a value
         System.out.println("Key for value 'Value3': " + table.getKey("Value3"));
 
-        // Check if a key exists in the hashtable
         System.out.println("Contains key key2: " + table.containsKey(key2));
 
     }
